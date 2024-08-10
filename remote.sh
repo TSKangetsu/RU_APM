bbcbbf288c88a52b57b3c37ff8dce2f34a9fb957
TSKangetsu <x403070567@live.com>
2024-08-10 13:32:39 +0800

    add sendonly

--- remote.sh
+++ remote.sh
@@ -8,6 +8,6 @@ echo "[$3-DEBUG] lanuch $3 Directly ..."
 touch .vscode/connect-ok
 if [ "$9" == "1" ]; then
     $5 ssh $1@$2 "$8 /usr/bin/gdbserver :9590 $7$3 $4"
-else
+elif [  "$9" == "2" ]; then
     $5 ssh $1@$2 "$8 $7$3 $4"
 fi
